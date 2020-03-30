451c1cedfa74067817813772dee96dbcbb32b1f6
JsssCode <gurupsdmail@gmail.com>
2020-03-31 01:09:02 +0300

    addition files etc

--- src/app/core/modules/material-imports.module.ts
+++ src/app/core/modules/material-imports.module.ts
@@ -4,8 +4,8 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
 //import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material';
-import { MatInputModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
 imports: [
